aac03dd7c01aebbb4157e656ff20d1c0fea2bacb
DIYA GARG <diyagarg018@gmail.com>
2025-10-30 23:25:08 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r')as file:
+            lines=file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                if requirement and requirement!='-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirememnt.txt not found")
+    return requirement_lst
+#if __name__ == "__main__":
+    print(get_requirements())
+setup(
+    name="networksecurity",
+    version="0.0.1",
+    author="diya",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)    
